3977118bf16adcab0b179a4559c9407fa6395a78
ArjavJ123 <75328060+ArjavJ123@users.noreply.github.com>
2022-07-26 15:06:40 +0530

    Create solution.cpp

---
+++ solution.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head == NULL) {
+            return false;
+        }
+        ListNode* p1 = head;
+        ListNode* p2 = head->next;
+        while (p1 != p2) {
+            if(p1->next == NULL|| p2 == NULL || p2->next == NULL) {
+                return false;
+            }
+            p1 = p1->next;
+            p2 = p2->next->next;
+            
+        }
+        return true;
+    }
+};
